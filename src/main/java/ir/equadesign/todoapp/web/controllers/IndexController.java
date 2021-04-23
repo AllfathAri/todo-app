6b3722aa52aab7acedf103dd7d71c530c46b9766
ehsan <sir.ehrp@gmail.com>
2021-04-24 01:23:49 +0430

    Added RUD operations to Controller :)

--- src/main/java/ir/equadesign/todoapp/web/controllers/IndexController.java
+++ src/main/java/ir/equadesign/todoapp/web/controllers/IndexController.java
@@ -6,6 +6,7 @@ import ir.equadesign.todoapp.services.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,7 +14,7 @@ import java.util.Set;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping({"/", "", "index", "/index.html"})
+@RequestMapping({"/", "", "index","/index", "/index.html"})
 public class IndexController {
 
     private final TaskService taskService;
@@ -21,5 +22,23 @@ public class IndexController {
     @GetMapping
     public ModelAndView index() {
         return new ModelAndView("index").addObject("tasks", taskService.findAll());
+    }
+
+    @GetMapping("/delete/{taskId}")
+    public String deleteTask(@PathVariable String taskId){
+        taskService.deleteTask(Integer.valueOf(taskId));
+        return "redirect:/index";
+    }
+
+    @GetMapping("/undone/{taskId}")
+    public String undoneTask(@PathVariable String taskId){
+        taskService.markAsUnDone(Integer.valueOf(taskId));
+        return "redirect:/index";
+    }
+
+    @GetMapping("/done/{taskId}")
+    public String doTask(@PathVariable String taskId){
+        taskService.markAsDone(Integer.valueOf(taskId));
+        return "redirect:/index";
     }
 }
