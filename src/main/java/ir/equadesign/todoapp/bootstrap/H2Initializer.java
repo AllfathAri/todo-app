b4000d05a24f12cf9ab2c4a908a4213584ae559c
ehsan <sir.ehrp@gmail.com>
2021-04-23 15:01:21 +0430

    Data initialized

---
+++ src/main/java/ir/equadesign/todoapp/bootstrap/H2Initializer.java
@@ -0,0 +1,42 @@
+package ir.equadesign.todoapp.bootstrap;
+
+import ir.equadesign.todoapp.domain.Category;
+import ir.equadesign.todoapp.domain.Task;
+import ir.equadesign.todoapp.repositories.CategoryRepository;
+import ir.equadesign.todoapp.repositories.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("h2")
+@Component
+@RequiredArgsConstructor
+public class H2Initializer implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final TaskRepository taskRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadData();
+    }
+
+    private void loadData() {
+
+        var allCategory = categoryRepository.save(Category.builder().category("all").build());
+        var importantCategory = categoryRepository.save(Category.builder().category("important").build());
+
+        for (int i = 0; i < 10; i++) {
+            var task = Task.builder().details("lorem ipsum dolor").build();
+            task.getCategories().add(allCategory);
+            taskRepository.save(task);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            var task = Task.builder().details("lorem ipsum dolor").build();
+            task.getCategories().add(importantCategory);
+            taskRepository.save(task);
+        }
+    }
+}
