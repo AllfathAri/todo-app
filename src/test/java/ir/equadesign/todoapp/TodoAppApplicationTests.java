816305e13ab810dd6344fc43606d5d6ba79ff5aa
ehsan <sir.ehrp@gmail.com>
2021-04-23 13:20:55 +0430

    Project init

---
+++ src/test/java/ir/equadesign/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.equadesign.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
